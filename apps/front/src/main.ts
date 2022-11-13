681b324ed8f2e75e687d2cebb3720b092c123b95
maxime <maxime.wets@iudo.co>
2022-11-13 16:00:53 +0100

    first commit

--- apps/front/src/main.ts
+++ apps/front/src/main.ts
@@ -1,4 +1,24 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App.vue';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import {
+  faHome,
+  faUser,
+  faUserPlus,
+  faSignInAlt,
+  faSignOutAlt,
+} from '@fortawesome/free-solid-svg-icons';
 
-createApp(App).mount('#app');
+library.add(faHome, faUser, faUserPlus, faSignInAlt, faSignOutAlt);
+
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia);
+app.component('font-awesome-icon', FontAwesomeIcon);
+
+app.mount('#app');
